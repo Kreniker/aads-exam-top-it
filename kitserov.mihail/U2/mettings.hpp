c7087bf14c167d9f16be9d1aedcbf72336246f44
Kreniker <kitserov.mike@gmail.com>
2026-06-25 13:36:37 +0300

    func for mettings

--- kitserov.mihail/U2/mettings.hpp
+++ kitserov.mihail/U2/mettings.hpp
@@ -15,6 +15,36 @@ namespace kitserov
     size_t size_;
     size_t capacity_;
   };
+  void addMeeting(MeetingsContainer& c, std::size_t id1, std::size_t id2, int dur) {
+    if (c.size_ == c.capacity_) {
+      std::size_t new_cap = (c.capacity_ == 0) ? 8 : c.capacity_ * 2;
+      Meeting* new_data = new Meeting[new_cap];
+      try {
+        for (std::size_t i = 0; i < c.size_; ++i) {
+          new_data[i] = c.data_[i];
+        }
+        new_data[c.size_] = Meeting{id1, id2, dur};
+      } catch (...) {
+        delete[] new_data;
+        throw;
+      }
+      delete[] c.data_;
+      c.data_ = new_data;
+      c.capacity_ = new_cap;
+    } else {
+      c.data_[c.size_] = Meeting{id1, id2, dur};
+    }
+    ++c.size_;
+  }
+  void removeSelfMeetings(MeetingsContainer& c) {
+    size_t write = 0;
+    for (size_t i = 0; i < c.size_; ++i) {
+      if (c.data_[i].id1_ != c.data_[i].id2_) {
+        c.data_[write++] = c.data_[i];
+      }
+    }
+    c.size_ = write;
+  }
 
 }
 
